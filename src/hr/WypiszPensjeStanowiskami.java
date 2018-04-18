313913c96a5f80ffeb70c3b13c19f54013449f27
kurs <kurs@10.0.2.15>
2018-04-18 16:59:23 +0200

    Początek wypisaNIA STANOWISK

--- src/hr/WypiszPensjeStanowiskami.java
+++ src/hr/WypiszPensjeStanowiskami.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.util.List;
 import java.util.Scanner;
 
-public class WypiszZeStanowisko {
+public class WypiszPensjeStanowiskami {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("podaj stanowisko pracownika");
-		String job = sc.nextLine();
+		
+		final String job1 ="Programmer";
+		final String job2 ="Sales";
+		final String job3 ="Public Accountant";
+		final String job4 ="Accounting Manager";
+		final String job5 = 
+		final String job6 = 
 		
 		File file = new File("pracownicy.csv");
 		System.out.println("Zaczynamy czytać plik");
@@ -20,8 +24,13 @@ public class WypiszZeStanowisko {
 		int i =0;
 		double pensja=0;
 		double pensjaProgramisty=0;
+		
+                
 		for (Employee employee : listaPracownikow) {
 			pensja +=employee.getSalary();
+			switch(job) {
+			case:
+			}
 			if(employee.getJobTitle().equalsIgnoreCase(job)) {
 				++i;
 			pensjaProgramisty+= employee.getSalary();
